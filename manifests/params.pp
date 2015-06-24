6a52e9c37fe46ccbe3ae742d94217312bafceb32
omc10 <technik@omc.net>
2015-06-24 09:26:59 +0200

    Add support for fedora 21
    Set correct params for Fedora 21.

--- manifests/params.pp
+++ manifests/params.pp
@@ -15,6 +15,14 @@ class stash::params {
         $service_file_location   = '/etc/init.d/stash'
         $service_file_template   = 'stash/stash.initscript.redhat.erb'
         $service_lockfile        = '/var/lock/subsys/stash'
+      } elsif $::operatingsystem == 'Fedora' {
+          if $::operatingsystemmajrelease == '21' {
+            $json_packages           = 'rubygem-json'
+            $service_file_location   = '/usr/lib/systemd/system/stash.service'
+            $service_file_template   = 'stash/stash.service.erb'
+            $service_lockfile        = '/var/lock/subsys/stash'
+          }
+
       } else {
         fail("${::operatingsystem} ${::operatingsystemmajrelease} not supported")
       }
